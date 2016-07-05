326c289ba799aed18f8c57e1db2ed31e64ecf1fa
kuroky123 <344854952@qq.com>
2016-07-05 12:08:24 +0800

    test

---
+++ html/fn_system.php
@@ -0,0 +1,10 @@
+<?php  
+  
+if(emptyempty($_SESSION['user'])){  
+      
+    header("Location:http://115.28.194.142/fn_wx_login.php");  
+}else{  
+    print_r($_SESSION['user']);  
+}  
+  
+?> 
