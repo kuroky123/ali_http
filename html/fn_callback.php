d7025336fd14fd22d677822df668612c8811a655
kuroky123 <344854952@qq.com>
2016-07-05 12:14:12 +0800

    test

--- html/fn_callback.php
+++ html/fn_callback.php
@@ -4,7 +4,7 @@ $appid = "wx83fbd1d95f61bb0c";
 $secret = "1e2fa5c5385c135a77bf64a6ec19c8a9";  
 $code = $_GET["code"];  
 $get_token_url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.$appid.'&secret='.$secret.'&code='.$code.'&grant_type=authorization_code';  
-  
+echo $code."\n";
 $ch = curl_init();  
 curl_setopt($ch,CURLOPT_URL,$get_token_url);  
 curl_setopt($ch,CURLOPT_HEADER,0);  
@@ -13,7 +13,7 @@ curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
 $res = curl_exec($ch);  
 curl_close($ch);  
 $json_obj = json_decode($res,true);  
-  
+echo $res."\n";
 //根据openid和access_token查询用户信息  
 $access_token = $json_obj['access_token'];  
 $openid = $json_obj['openid'];  
