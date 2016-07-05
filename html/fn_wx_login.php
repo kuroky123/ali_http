5c94f5e34699aa841ccc782c627866d7316a3822
kuroky123 <344854952@qq.com>
2016-07-05 14:37:05 +0800

    test

--- html/fn_wx_login.php
+++ html/fn_wx_login.php
@@ -1,7 +1,7 @@
 <?php  
   
     $appid = "wx83fbd1d95f61bb0c";  
-    $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$appid.'&redirect_uri=http%3a%2f%2f115.28.194.142%2ffn_callback.php&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';  
+    $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$appid.'&redirect_uri=http%3a%2f%2f115.28.194.142%2ffn_callback.php&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect';  
     header("Location:".$url);  
   
 ?>  
